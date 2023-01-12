026b2aa2098c9d62b68b783fe2a8644769646f81
CocoCSforever <yijia.ma18@gmail.com>
2023-01-12 13:13:05 -0800

    Jan 12th

---
+++ src/FractionTest.java
@@ -0,0 +1,28 @@
+package src;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.Fraction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class FractionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testEquals() {
+        Fraction fraction1 = new Fraction(2,3);
+        Fraction fraction2 = new Fraction(2,3);
+
+        assertEquals(fraction1, fraction2);
+
+        fraction1= new Fraction(3,5);
+        fraction2=new Fraction(4,6);
+
+        assertNotEquals(fraction1, fraction2);
+    }
+}
